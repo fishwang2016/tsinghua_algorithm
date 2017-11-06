96f8202a9748b3c6825cc2e1414d6d452accf6cd
ZHENYU WANG <zhenyu2004@hotmail.com>
2017-11-06 16:29:18 +0800

    The first problem-range

---
+++ range.cpp
@@ -0,0 +1,75 @@
+/*
+**Descriptioin
+Let S be a set of n integral points on the x-axis. For each given interval [a, b], you are asked to count the points lying inside.
+
+Input
+The first line contains two integers: n (size of S) and m (the number of queries).
+
+The second line enumerates all the n points in S.
+
+Each of the following m lines consists of two integers a and b and defines an query interval [a, b].
+
+Output
+The number of points in S lying inside each of the m query intervals.
+
+Example
+Input
+
+5 2
+1 3 7 9 11
+4 6
+7 12
+Output
+
+0
+3
+Restrictions
+0 <= n, m <= 5 * 10^5
+
+For each query interval [a, b], it is guaranteed that a <= b.
+
+Points in S are distinct from each other.
+
+Coordinates of each point as well as the query interval boundaries a and b are non-negative integers not greater than 10^7.
+
+Time: 2 sec
+
+Memory: 256 MB
+
+**
+**
+*/
+#include <iostream>
+
+
+
+using namespace std;
+
+int main(){
+
+    long n;
+    long m;
+
+    long a, b;
+    long data[100000];
+    cin >> n ;
+    cin >> m;
+    for (int i=0;i<n;i++)
+        cin >> data[i];
+
+    for (int i=0; i<m;i++){
+        cin >> a;
+        cin >> b;
+        long count = 0;
+        for (int i=0; i< n ;i++){
+            if (data[i]>=a && data[i]<=b)
+                count++;
+        }
+
+        cout << count << "\n";
+
+    }
+
+
+   return 0;
+}
